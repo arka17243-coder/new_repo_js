fa03d983e9298d039927ff0d4381c1ffc8314de0
Arka Ghosh <arka17243@gmail.com>
2026-06-23 09:37:20 +0000

    Types of datatypes

--- basics/var2.js
+++ basics/var2.js
@@ -4,4 +4,36 @@ let newscore = Number(score)
 //console.log(newscore)
 let somenumber = 33
 let st1 = String(somenumber)
-console.log(typeof(st1))
+//console.log(typeof(st1))
+let val = 9
+let newval = -val
+//console.log(newval)
+// console.log("1"+3)
+// console.log(+true)
+let num1,num2,num3
+console.log(null>=0)
+console.log(0==0)
+console.log(null<0)
+/*# 
+
+How data is stored and accessed in memory 
+Primitive 
+call by value, 
+// 7 types : String ,Number,Boolean,null,undefined,Symbol,Bigint
+Non primitive  call by reference
+Array, Objects , functions
+*/
+
+
+//array example
+let arr = [2,4,5,7]
+//object
+let myobj={
+    name : "ARKA",
+    age : 22,
+}
+//functions
+const myfunc  = function()
+{
+    console.log(542);
+}
