d748dcc852604ca6cf51a2d2ce1b79882c522516
Arka Ghosh <arka17243@gmail.com>
2026-06-25 09:31:19 +0000

    New Time object included

---
+++ basics/var5.js
@@ -0,0 +1,12 @@
+//Date and time
+
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//let mycreateddate = new Date(2023,0,23)
+let mycreateddate = new Date("01-14-2023")
+//console.log(mycreateddate.toDateString())
+let myTimestamp = Date.now()
+//console.log(mycreateddate.toLocaleString())
+console.log(myTimestamp)
