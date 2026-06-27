a0c44315e0f5f4cb9fb9f8163bdaa235522dd072
Arka Ghosh <arka17243@gmail.com>
2026-06-27 07:03:13 +0000

    time edited

--- basics/var5.js
+++ basics/var5.js
@@ -1,12 +1,31 @@
 //Date and time
 
+/* several string representation of date */
+
+
 let myDate = new Date()
 //console.log(myDate.toString());
 //console.log(myDate.toDateString());
 //console.log(myDate.toLocaleDateString());
 //let mycreateddate = new Date(2023,0,23)
 let mycreateddate = new Date("01-14-2023")
-//console.log(mycreateddate.toDateString())
-let myTimestamp = Date.now()
 //console.log(mycreateddate.toLocaleString())
-console.log(myTimestamp)
+//console.log(mycreateddate.toDateString())
+let mynewdate = new Date("2023-01-14")
+//console.log(mynewdate.toLocaleString())
+
+/* Timestamp  */
+
+let myTimestamp = Date.now() //ms value number does not take parameter
+//console.log(myTimestamp)
+//console.log(mycreateddate.getTime()) convert to number in ms 
+//console.log(Math.floor(Date.now()/1000));
+
+/* get */
+
+let newdate = new Date()
+//console.log(newdate.getDay())
+console.log(newdate.getFullYear())
+
+
+
