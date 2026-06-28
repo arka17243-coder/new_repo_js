fc5da0d1b8b864761ef21bdec41014759f8eed14
Arka Ghosh <arka17243@gmail.com>
2026-06-28 05:30:41 +0000

    new array object included

---
+++ basics_02/arrays.js
@@ -0,0 +1,18 @@
+//array 
+
+const myarr = [0,1,2,3,4];
+const arr1 = new Array(1,2,5,6)
+//console.log(arr1)
+
+/* Array Methods */
+
+// arr1.push(87)
+// // arr1.pop()
+
+// arr1.unshift(9)
+// arr1.shift()
+// arr1.shift()
+// console.log(arr1.includes(3)) 
+console.log(arr1.indexOf(3))
+
+//console.log()
