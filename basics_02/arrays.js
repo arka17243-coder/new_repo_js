017ac483999f58cf58ee93f2d049b794676acfe9
Arka Ghosh <arka17243@gmail.com>
2026-06-30 09:52:50 +0000

    array  part 1 included

--- basics_02/arrays.js
+++ basics_02/arrays.js
@@ -15,5 +15,30 @@ const arr1 = new Array(1,2,5,6)
 // console.log(arr1.includes(3)) 
 //console.log(arr1.indexOf(3))
 const arr2 = arr1.join("-")
-console.log(arr1)
-console.log(arr2)
+// console.log(arr1)
+// console.log(arr2)
+
+//  Difference between splice and slice functions
+
+const newarr = new Array(1,3,4,5,6,7)
+console.log("A: ", newarr);
+
+// slice action
+console.log(newarr.slice(0,3));
+console.log("B: ",newarr);
+// does not change the main array ,
+// return a copy of a section of the array  containing the elements starting from start index and ending in the last index but excluding the last index
+// if starting not mentioned start from  0 
+//  if ending not mentioned then ended at the last index 
+// (4,last index) (4,) (4)
+
+//splice function 
+console.log(newarr.splice(1))
+console.log("C: ",newarr)
+
+// The main array does not have the deleted elements 
+// return a copy of a section of the array  containing the elements starting from start index and ending in the last index but including the last index
+// if ending not mentioned end index would be the last index 
+// (4,last index) (4,) (4)
+
+
