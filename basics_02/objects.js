1b8f1388131d12c5e65ac505d593c51f70a07ca3
Arka Ghosh <arka17243@gmail.com>
2026-07-02 07:45:03 +0000

    object intro included

--- basics_02/objects.js
+++ basics_02/objects.js
@@ -1,2 +1,11 @@
  // singleton 
  
+//  object literals
+const JsUser = {
+    name : "Arka",
+    age : 21,
+    "location": "Kolkata",
+    email: "raka@mail.com",
+    lastassesday : ["Monday","wednesday"]
+}
+console.log(JsUser.email) //to access object elements
