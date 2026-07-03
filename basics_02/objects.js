72879e3bc8b3e61b2d7922b041710e69acc4fd48
Arka Ghosh <arka17243@gmail.com>
2026-07-03 03:48:59 +0000

    object included

--- basics_02/objects.js
+++ basics_02/objects.js
@@ -8,4 +8,25 @@ const JsUser = {
     email: "raka@mail.com",
     lastassesday : ["Monday","wednesday"]
 }
-console.log(JsUser.email) //to access object elements
+
+//two ways to access object elements
+
+// console.log(JsUser.email)
+// console.log(JsUser["age"])
+
+// for symbol 
+const mysym  = Symbol("key1")
+const newobject = {
+[mysym] : 21
+}
+// console.log(newobject[mysym])
+
+JsUser.name = "Eswar"
+//Object.freeze(JsUser)
+JsUser.location = "kurusaki" 
+// console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello Javascript")
+}
+console.log(JsUser.greeting())
