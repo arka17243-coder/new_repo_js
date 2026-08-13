ddee02428d91301527506565f3816a02180b5ef9
Arka Ghosh <arka17243@gmail.com>
2026-08-13 09:42:31 +0000

    scope of  variable is declared

---
+++ basics_03/scope.js
@@ -0,0 +1,16 @@
+function one()
+{
+    const username ="arka" //global variable for two function
+    function two()
+    {
+        const website="youtube"
+        console.log(username)
+    }
+    two()
+}
+//one()
+const  addtwo =function(num)
+{
+    return num +2
+}
+console.log(addtwo(3))
