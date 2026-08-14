379db8244248602be80e4c00b459f4e3cf8221f5
Arka Ghosh <arka17243@gmail.com>
2026-08-14 10:10:26 +0000

    new control folder added

---
+++ control_flow/one.js
@@ -0,0 +1,23 @@
+// if(2==2)
+// {
+// console.log("it is true")
+// }else{
+//     console.log("This is false")
+// }
+// < >  <= >= == 
+
+const balance =500
+// if( balance >400) console.log("tested");
+if(balance<500 )
+{
+
+}
+else if(balance<500)
+{
+
+}
+else{
+
+}
+if(one && two) {}
+if (one || two){}
